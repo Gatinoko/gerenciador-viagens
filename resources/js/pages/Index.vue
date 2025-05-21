6c18623c1dd98fe99f660248280e70762523be58
Gabriel Tinoco <gabrielslai@gmail.com>
2025-05-21 18:36:43 -0300

    feat: Concluded Vue + Inertia.js setup

---
+++ resources/js/pages/Index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="p-6 text-gray-900">
+        <h1>Welcome to {{ appName }}</h1>
+    </div>
+</template>
+
+<script setup>
+defineProps(["appName"]);
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Index', [
+        'appName' => config('app.name'),
+    ]);
 });
