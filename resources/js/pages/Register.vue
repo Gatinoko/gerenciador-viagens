23eb985c57ddcaa643cde7bdd1f5838be91ddab5
Gabriel Tinoco <gabrielslai@gmail.com>
2025-05-22 15:38:32 -0300

    feat: Created basic register form at register page

--- resources/js/pages/Register.vue
+++ resources/js/pages/Register.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { Link } from "@inertiajs/vue3";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import AppLayout from "@/layouts/AppLayout.vue";
 
 defineProps({ appName: String });
@@ -6,6 +9,39 @@ defineProps({ appName: String });
 
 <template>
     <AppLayout>
-        <h2>Register page</h2>
+        <div
+            class="p-10 w-200 self-center bg-neutral-300 rounded-xl border-1 border-neutral-400"
+        >
+            <!-- Register form -->
+            <form>
+                <div>
+                    <Label for="email" class="text-right"> Email </Label>
+                    <Input
+                        class="col-span-3"
+                        id="email"
+                        placeholder="exemplo@gmail.com"
+                    />
+                </div>
+                <div>
+                    <Label for="senha" class="text-right"> Senha </Label>
+                    <Input
+                        class="col-span-3"
+                        id="password"
+                        placeholder="senha123"
+                    />
+                </div>
+            </form>
+
+            <!-- Register button -->
+            <Button>Registrar</Button>
+
+            <!-- Auxiliary text -->
+            <p>
+                Já possui uma conta?
+                <Link href="./register" class="text-blue-400 underline"
+                    >Faça login aqui</Link
+                >
+            </p>
+        </div>
     </AppLayout>
 </template>
