80742e0493b276f97f7b2f19a275593afc990822
Gabriel Tinoco <gabrielslai@gmail.com>
2025-05-22 15:38:21 -0300

    feat: Created basic login form at login page

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -1,11 +1,47 @@
 <script setup lang="ts">
 import AppLayout from "@/layouts/AppLayout.vue";
+import { Link } from "@inertiajs/vue3";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 defineProps({ appName: String });
 </script>
 
 <template>
     <AppLayout>
-        <h2>Login page</h2>
+        <div
+            class="p-10 w-200 self-center bg-neutral-300 rounded-xl border-1 border-neutral-400"
+        >
+            <!-- Register form -->
+            <form>
+                <div>
+                    <Label for="email" class="text-right"> Email </Label>
+                    <Input
+                        class="col-span-3"
+                        id="email"
+                        placeholder="exemplo@gmail.com"
+                    />
+                </div>
+                <div>
+                    <Label for="senha" class="text-right"> Senha </Label>
+                    <Input
+                        class="col-span-3"
+                        id="password"
+                        placeholder="senha123"
+                    />
+                </div>
+            </form>
+
+            <!-- Register button -->
+            <Button>Login</Button>
+
+            <!-- Auxiliary text -->
+            <p>
+                Não possui uma conta?
+                <Link href="./register" class="text-blue-400 underline"
+                    >Registre-se aqui</Link
+                >
+            </p>
+        </div>
     </AppLayout>
 </template>
