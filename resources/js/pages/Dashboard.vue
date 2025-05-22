a22235fe0adf595ed5a001eaebf396aaf1fd1e48
Gabriel Tinoco <gabrielslai@gmail.com>
2025-05-22 15:00:40 -0300

    feat: Added and implemented shadcn/vue Table component

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
 import { Button } from "@/components/ui/button";
+import {
+    Table,
+    TableCaption,
+    TableHeader,
+    TableRow,
+    TableHead,
+    TableBody,
+    TableCell,
+} from "@/components/ui/table";
 import AppLayout from "@/layouts/AppLayout.vue";
 
 defineProps({ appName: String });
@@ -8,5 +17,24 @@ defineProps({ appName: String });
 <template>
     <AppLayout>
         <h1>Dashboard</h1>
+        <Table>
+            <TableCaption>A list of your recent invoices.</TableCaption>
+            <TableHeader>
+                <TableRow>
+                    <TableHead class="w-[100px]"> Invoice </TableHead>
+                    <TableHead>Status</TableHead>
+                    <TableHead>Method</TableHead>
+                    <TableHead class="text-right"> Amount </TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                <TableRow>
+                    <TableCell class="font-medium"> INV001 </TableCell>
+                    <TableCell>Paid</TableCell>
+                    <TableCell>Credit Card</TableCell>
+                    <TableCell class="text-right"> $250.00 </TableCell>
+                </TableRow>
+            </TableBody>
+        </Table>
     </AppLayout>
 </template>
