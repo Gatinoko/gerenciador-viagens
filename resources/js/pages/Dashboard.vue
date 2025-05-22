456153a5b9ac1dac9892a7093ddb01c7f2918a7f
Gabriel Tinoco <gabrielslai@gmail.com>
2025-05-22 15:15:00 -0300

    feat: Added and implemented shadcn/vue Dialog and Input components

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -9,6 +9,16 @@ import {
     TableBody,
     TableCell,
 } from "@/components/ui/table";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
 import AppLayout from "@/layouts/AppLayout.vue";
 
 defineProps({ appName: String });
@@ -36,5 +46,43 @@ defineProps({ appName: String });
                 </TableRow>
             </TableBody>
         </Table>
+
+        <Dialog>
+            <DialogTrigger as-child>
+                <Button>Novo pedido de viagem</Button>
+            </DialogTrigger>
+            <DialogContent class="sm:max-w-[425px]">
+                <DialogHeader>
+                    <DialogTitle>Edit profile</DialogTitle>
+                    <DialogDescription>
+                        Make changes to your profile here. Click save when
+                        you're done.
+                    </DialogDescription>
+                </DialogHeader>
+                <div class="grid gap-4 py-4">
+                    <div class="grid grid-cols-4 items-center gap-4">
+                        <Label for="name" class="text-right"> Name </Label>
+                        <Input
+                            id="name"
+                            value="Pedro Duarte"
+                            class="col-span-3"
+                        />
+                    </div>
+                    <div class="grid grid-cols-4 items-center gap-4">
+                        <Label for="username" class="text-right">
+                            Username
+                        </Label>
+                        <Input
+                            id="username"
+                            value="@peduarte"
+                            class="col-span-3"
+                        />
+                    </div>
+                </div>
+                <DialogFooter>
+                    <Button type="submit"> Save changes </Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
     </AppLayout>
 </template>
