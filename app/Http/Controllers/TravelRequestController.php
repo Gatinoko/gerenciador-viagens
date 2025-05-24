8b96e21d637a5a9bd9bbf23e26cbb7ceb3079fcc
Gabriel Tinoco <gabrielslai@gmail.com>
2025-05-24 16:45:41 -0300

    feat: Created initial TravelRequestController and TravelRequest model

---
+++ app/Http/Controllers/TravelRequestController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TravelRequestController extends Controller
+{
+    public function createTravelRequest(Request $request) { }
+
+    public function updateTravelRequest(Request $request) { }
+
+    public function cancelTravelRequest(Request $request) { }
+
+    public function getUserTravelRequests(Request $request) { }
+}
