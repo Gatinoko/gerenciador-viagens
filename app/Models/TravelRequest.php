ef9da2d440d716b282edaf90f5f4f11a2939cb6d
Gabriel Tinoco <gabrielslai@gmail.com>
2025-05-25 00:56:34 -0300

    refactor: Fixed TravelRequest model not extending Model and solicitor_id field typo

--- app/Models/TravelRequest.php
+++ app/Models/TravelRequest.php
@@ -4,9 +4,10 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 
-class TravelRequest
+class TravelRequest extends Model
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
@@ -17,7 +18,7 @@ class TravelRequest
      * @var list<string>
      */
     protected $fillable = [
-        'solicitor',
+        'solicitor_id',
         'destination',
         'departure_date',
         'return_date',
