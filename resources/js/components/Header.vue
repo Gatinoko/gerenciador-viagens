e94d4c286c745c03a2596e9aef759f667181bfda
Gabriel Tinoco <gabrielslai@gmail.com>
2025-05-25 21:13:22 -0300

    feat: Cleaned up Header and Navigation component styles

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -6,8 +6,10 @@ const props = defineProps({
 
 <template>
     <header
-        class="bg-neutral-400 px-40 py-5 mb-10 border-b-1 border-neutral-500"
+        class="bg-primary/75 px-12 py-5 mb-10 border-b-1 border-primary/50 shadow-md"
     >
-        <h1 class="text-5xl">{{ text }}</h1>
+        <h1 class="text-5xl font-semibold text-secondary">
+            {{ text }}
+        </h1>
     </header>
 </template>
--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -2,6 +2,7 @@
 import { Button } from "@/components/ui/button";
 import { Link, usePage } from "@inertiajs/vue3";
 import { computed } from "vue";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const page = usePage();
 const user = computed(() => page.props.auth?.user);
@@ -11,40 +12,77 @@ const { appName, errors } = defineProps({ appName: String, errors: Object });
 
 <template>
     <nav
-        class="bg-neutral-500 p-2 flex justify-between border-b-1 border-neutral-600"
+        class="bg-primary py-2 px-12 flex border-b-1 gap-6 border-neutral-600 shadow-md"
     >
-        <ul class="flex gap-1">
-            <li>
-                <Link href="./dashboard">
-                    <Button>Dashboard</Button>
-                </Link>
-            </li>
-            <li>
-                <Link href="/">
-                    <Button>Index</Button>
-                </Link>
-            </li>
-        </ul>
+        <!-- Application name -->
+        <h1
+            class="self-center font-semibold text-primary-foreground text-nowrap select-none"
+        >
+            Gerenciador de Viagens &copy
+        </h1>
 
-        <span class="text-red-600" v-if="user">{{ user.email }}</span>
-        <span class="text-red-600" v-if="user">{{ user.name }}</span>
+        <div class="flex w-full justify-between">
+            <ul class="flex gap-1">
+                <li>
+                    <Link href="/">
+                        <Button
+                            size="sm"
+                            class="bg-transparent hover:bg-secondary/10"
+                            >Inicio</Button
+                        >
+                    </Link>
+                </li>
+                <li>
+                    <Link v-if="user" href="./dashboard">
+                        <Button
+                            size="sm"
+                            class="bg-transparent hover:bg-secondary/10"
+                            >Dashboard</Button
+                        >
+                    </Link>
+                </li>
+            </ul>
 
-        <ul class="flex gap-1">
-            <li>
-                <Link v-if="user" href="/logout" method="post">
-                    <Button>Logout</Button>
-                </Link>
-            </li>
-            <li>
-                <Link href="/login">
-                    <Button>Login</Button>
-                </Link>
-            </li>
-            <li>
-                <Link href="/register">
-                    <Button>Register</Button>
-                </Link>
-            </li>
-        </ul>
+            <!-- <span class="text-red-600" v-if="user">{{ user.email }}</span>
+            <span class="text-red-600" v-if="user">{{ user.name }}</span> -->
+
+            <ul class="flex gap-1">
+                <li>
+                    <Link v-if="user" href="/logout" method="post">
+                        <Button
+                            size="sm"
+                            class="bg-transparent hover:bg-secondary/10"
+                            >Logout</Button
+                        >
+                    </Link>
+                </li>
+                <li>
+                    <Link v-if="!user" href="/login">
+                        <Button
+                            size="sm"
+                            class="bg-transparent hover:bg-secondary/10"
+                            >Login</Button
+                        >
+                    </Link>
+                </li>
+                <li>
+                    <Link v-if="!user" href="/register">
+                        <Button
+                            size="sm"
+                            class="bg-transparent hover:bg-secondary/10"
+                            >Registro</Button
+                        >
+                    </Link>
+                </li>
+
+                <Avatar v-if="user">
+                    <AvatarImage
+                        src="https://github.com/unovue.png"
+                        alt="@unovue"
+                    />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </ul>
+        </div>
     </nav>
 </template>
