28d689b33a67fb716e97184a3c2281b447e4dd40
Gabriel Tinoco <gabrielslai@gmail.com>
2025-05-26 22:41:57 -0300

    feat: Added missing route guards

--- routes/web.php
+++ routes/web.php
@@ -12,15 +12,15 @@ use Illuminate\Http\Request;
 // Page routes
 Route::get('/', [PagesController::class, 'showIndex'])->name("show.index");
 Route::get('/dashboard', [PagesController::class, 'showDashboard'])->name("show.dashboard")->middleware(['auth']);
-Route::get('/login', [PagesController::class, 'showLogin'])->name("show.login");
-Route::get('/register', [PagesController::class, "showRegister"])->name("show.register");
+Route::get('/login', [PagesController::class, 'showLogin'])->name("show.login")->middleware(['guest']);
+Route::get('/register', [PagesController::class, "showRegister"])->name("show.register")->middleware(['guest']);
 
 // Auth routes
-Route::post('/login', [AuthController::class, 'login'])->name("login");
-Route::post('/register', [AuthController::class, "register"])->name("register");
+Route::post('/login', [AuthController::class, 'login'])->name("login")->middleware(['guest']);
+Route::post('/register', [AuthController::class, "register"])->name("register")->middleware(['guest']);
 Route::post('/logout', [AuthController::class, "logout"])->name("logout")->middleware(['auth']);
 
 // Travel request routes
-Route::post('/travelRequest/create', [TravelRequestController::class, "createTravelRequest"])->name("createTravelRequest");
-Route::get('/travelRequest/get', [TravelRequestController::class, "getUserTravelRequest"])->name("getUserTravelRequest");
-Route::post('/travelRequest/updateStatus', [TravelRequestController::class, "updateTravelRequestStatus"])->name("updateTravelRequestStatus");
+Route::post('/travelRequest/create', [TravelRequestController::class, "createTravelRequest"])->name("createTravelRequest")->middleware(['auth']);;
+Route::get('/travelRequest/get', [TravelRequestController::class, "getUserTravelRequest"])->name("getUserTravelRequest")->middleware(['auth']);;
+Route::post('/travelRequest/updateStatus', [TravelRequestController::class, "updateTravelRequestStatus"])->name("updateTravelRequestStatus")->middleware(['auth']);;
