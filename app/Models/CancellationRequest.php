6bec2b7ad9b4412cd3dfe5b376deb6f730c8f2a9
Gabriel Tinoco <gabrielslai@gmail.com>
2025-06-05 16:28:48 -0300

    feat: Created CancellationRequest model and respective migrations

---
+++ app/Models/CancellationRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class CancellationRequest extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'request_id',
+        'message',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [ ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [ ];
+    }
+
+    /**
+     * Defines relationship with TravelRequest model.
+     */
+    public function travelRequest() {
+        return $this->belongsTo(TravelRequest::class, 'request_id', 'id');
+    }
+}
