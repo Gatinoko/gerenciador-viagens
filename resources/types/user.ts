f82ca50a43ea1bb804b1ae231ad693167b31a25b
Gabriel Tinoco <gabrielslai@gmail.com>
2025-07-20 20:04:09 -0300

    feat: Added extra fields to User type and created UserRole type

--- resources/types/user.ts
+++ resources/types/user.ts
@@ -1,6 +1,12 @@
+import type { UserRole as Role } from "./UserRole";
+
 export type User = {
     id: number;
     name: string;
     email: string;
+    email_verified_at: string;
+    user_role: Role;
     password: string;
+    created_at: string;
+    updated_at: string;
 };
